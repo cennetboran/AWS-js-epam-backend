2130fbf83bf398ebb16107c70ca3cce371893874
cennet boran <cenne boran22@gmail.com>
2022-07-24 15:11:10 +0300

    task-3

--- prudct-service/handler.js
+++ prudct-service/handler.js
@@ -3,16 +3,12 @@
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
+    body: 
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        productName: 'Book',
+        price: 123,
       },
-      null,
-      2
-    ),
+    
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
